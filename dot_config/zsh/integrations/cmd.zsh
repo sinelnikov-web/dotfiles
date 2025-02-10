efb9351a4052ae9502a5109deaa5faaf06b9d7db
sinelnikov-web <sinelnikov-web@mail.ru>
2025-02-10 19:41:55 +0300

    remove thefuck

--- dot_config/zsh/integrations/cmd.zsh
+++ dot_config/zsh/integrations/cmd.zsh
@@ -1,7 +1,5 @@
 zsh-defer source "$ZSH/plugins/you-should-use/you-should-use.plugin.zsh"
 
-zsh-defer source "$ZSH/plugins/thefuck/thefuck.plugin.zsh"
-
 zsh-defer source "$ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
 zsh-defer source "$ZSH/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
 zsh-defer source "$ZSH/plugins/zsh-syntax-highlighting/themes/$CATPPUCCIN_FLAVOR.zsh"
